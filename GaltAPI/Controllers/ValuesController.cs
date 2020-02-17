e82b0f299d7e570c447e47bf7c499dfc012de5af
Your Name <cjbryant1@crimson.ua.edu>
2020-02-17 09:07:33 -0600

    Added delete function

--- GaltAPI/Controllers/ValuesController.cs
+++ GaltAPI/Controllers/ValuesController.cs
@@ -104,9 +104,36 @@ namespace GaltAPI.Controllers
         }
 
         // DELETE api/values/5
-        public void Delete(int id)
+        public List<results> Delete(int id)
         {
-            
+            MySqlConnection conn = WebApiConfig.conn();
+
+            MySqlCommand query = conn.CreateCommand();
+
+            query.CommandText = "DELETE pharmacyName, pharmacyAddress, pharmacyCategory FROM locations WHERE id = @id";
+
+            query.Parameters.AddWithValue("@id", id);
+
+            var results = new List<results>();
+
+            try
+            {
+                conn.Open();
+            }
+
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                results.Add(new results(null, null, null, ex.ToString()));
+            }
+
+            MySqlDataReader fetch_query = query.ExecuteReader();
+
+            while (fetch_query.Read())
+            {
+                results.Add(new results(fetch_query["pharmacyName"].ToString(), fetch_query["pharmacyAddress"].ToString(), fetch_query["pharmacyCategory"].ToString(), null));
+            }
+
+            return results;
         }
     }
 }
